d7fa59495717da3d046276a038af5f3de6f30e27
null <tony_hu@live.com>
2020-06-07 14:21:58 +0800

    Update repl.js

--- misc/repl.js
+++ misc/repl.js
@@ -191,11 +191,14 @@ function makeCompletion(event, packet) {
             var pre = match_[1]
             var post = match_[3]
 
+            try
+            {
             startIndex = semanticSegment.exec(messagestr).index
             var noneEvalPre = messagestr.substring(0, startIndex)
 
             endIndex = startIndex + match_[0].length
             var noneEvalPost = messagestr.substring(endIndex, messagestr.length)
+            } catch(e) {}
 
         }
 
