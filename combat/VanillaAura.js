4876b5f8f7f7cd41914ef1289e3d4bba7fafe781
null <register_for_bonic@163.com>
2021-01-28 02:23:45 +0800

    Update VanillaAura.js

--- combat/VanillaAura.js
+++ combat/VanillaAura.js
@@ -6,7 +6,6 @@
     authors: ["commandblock2"]
 })).import("Core.lib")
 
-
 var isEnemy
 var canBlock
 module =
@@ -17,6 +16,10 @@ module =
     category: "combat",
 
     onUpdate: function () {
+        
+        if (RandomUtils.nextFloat(0,1) > 0.8)
+            return
+        
         target = Java.from(mc.theWorld.loadedEntityList)
             .filter(function (e) { return isEnemy.invoke(KillAuraModule, e) })
             .sort(function (lhs, rhs) { return mc.thePlayer.getDistanceToEntity(lhs) - mc.thePlayer.getDistanceToEntity(rhs) })[0]
@@ -24,9 +27,9 @@ module =
         if(!target) return
 
 
-        if(mc.thePlayer.getHeldItem().getItem() instanceof ItemSword) mc.getNetHandler().addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN))
+        if(mc.thePlayer.getHeldItem() && mc.thePlayer.getHeldItem().getItem() instanceof ItemSword) mc.getNetHandler().addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN))
         mc.getNetHandler().addToSendQueue(new C02PacketUseEntity(target, C02PacketUseEntity.Action.ATTACK))
-        if(mc.thePlayer.getHeldItem().getItem() instanceof ItemSword) mc.getNetHandler().addToSendQueue(new C08PacketPlayerBlockPlacement(mc.thePlayer.inventory.getCurrentItem()))
+        if(mc.thePlayer.getHeldItem() && mc.thePlayer.getHeldItem().getItem() instanceof ItemSword) mc.getNetHandler().addToSendQueue(new C08PacketPlayerBlockPlacement(mc.thePlayer.inventory.getCurrentItem()))
     },
 
     onEnable: function () {
